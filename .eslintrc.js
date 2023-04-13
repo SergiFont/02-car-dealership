6aacb6d51435cc2e14247353adfccb3d951b940b
SergiFont <sfontsmx2010@gmail.com>
2023-04-13 18:20:27 +0200

    starting first Nest project

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+};
+/**
+ * Archivo de configuracion del linter. El linter sigue una serie de buenas prácticas, para cuando estemos configurando
+ * nuestro código, hacer que este siga unas reglas estándar. Son sugerencias de buenas prácticas, en resumen.
+ */
